aef065091dd6093cda2fc01949bab6c8f86c8497
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-13 19:26:04 +0400

    feat: создал класс DomElement

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+class DomElement {
+  constructor(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+  }
+}
