404675c147aea44d142f11f4e7e75fad425d0d44
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-13 19:43:14 +0400

    feat: создал createElem

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ class DomElement {
     this.bg = bg;
     this.fontSize = fontSize;
   }
+
+  createElem() {
+    let elem;
+    if (this.selector.substr(0, 1) === '.') {
+      
+      elem = document.createElement('div');
+      elem.classList.add(this.selector.slice(1));
+      
+    } else if (this.selector.substr(0, 1) === '#') {
+      elem = document.createElement('p');
+      elem.setAttribute('id', this.selector.slice(1));
+      
+  
+    }
+  }
 }
